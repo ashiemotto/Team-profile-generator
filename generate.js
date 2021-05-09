f49a706cf2fe5ae3d4a1fb3fc976da512974fef3
Ashiemotto <ashheadley@gmail.com>
2021-05-09 18:32:39 -0400

    new code

--- generate.js
+++ generate.js
@@ -17,18 +17,53 @@
                      <h1>My team</h1>
                 </div>
             </div>
-            <div class="container">
+            <div class="containermanager">
+            <div  style="max-width: 18rem; box-shadow: 5px 10px;">
+                <div class="card-header card text-white bg-primary "style ="font-size:25px; font-weight:bold;">${data.employee} Employee</div>
+                <div class="card-body card text-dark bg-light ">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${data.idnumber1}</li>
+                        <li class="list-group-item">Email:<a href="mailto:${data.email1}">${data.email1}</a></li>
+                    </ul>
+                </div>
+              </div>
+          </div>
+            <div class="containermanager">
                 <div  style="max-width: 18rem; box-shadow: 5px 10px;">
-                    <div class="card-header card text-white bg-primary "style ="font-size:25px; font-weight:bold;">${data.manager} Employee</div>
+                    <div class="card-header card text-white bg-primary "style ="font-size:25px; font-weight:bold;">${data3.manager} Employee</div>
                     <div class="card-body card text-dark bg-light ">
                         <ul class="list-group">
-                            <li class="list-group-item">ID: ${data.idnumber}</li>
-                            <li class="list-group-item">Email:<a href="mailto:${data.email}">${data.email}</a></li>
-                            <li class="list-group-item">Office: ${data.office}</li>
+                            <li class="list-group-item">ID: ${data3.idnumber2}</li>
+                            <li class="list-group-item">Email:<a href="mailto:${data3.email2}">${data3.email2}</a></li>
+                            <li class="list-group-item">Office: ${data3.office}</li>
                         </ul>
                     </div>
                   </div>
               </div>
+              <div class="containerengineer">
+              <div  style="max-width: 18rem; box-shadow: 5px 10px;">
+                  <div class="card-header card text-white bg-primary "style ="font-size:25px; font-weight:bold;">${data4.engineer} Employee</div>
+                  <div class="card-body card text-dark bg-light ">
+                      <ul class="list-group">
+                          <li class="list-group-item">ID: ${data4.idnumber3}</li>
+                          <li class="list-group-item">Email:<a href="mailto:${data4.email3}">${data4.email3}</a></li>
+                          <li class="list-group-item">Github: ${data4.github}</li>
+                      </ul>
+                  </div>
+                </div>
+            </div>
+            <div class="containereintern">
+            <div  style="max-width: 18rem; box-shadow: 5px 10px;">
+                <div class="card-header card text-white bg-primary "style ="font-size:25px; font-weight:bold;">${data5.intern} Employee</div>
+                <div class="card-body card text-dark bg-light ">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${data5.idnumber4}</li>
+                        <li class="list-group-item">Email:<a href="mailto:${data5.email4}">${data5.email4}</a></li>
+                        <li class="list-group-item">School: ${data5.school}</li>
+                    </ul>
+                </div>
+              </div>
+          </div>
     
            <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
--- index.js
+++ index.js
@@ -3,11 +3,65 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 const generateManager = require('./generate');
+let employ = []
 
-// questons for employee
-const writeFileAsync = util.promisify(fs.writeFile);
+// questons for employee number one
+ const writeToFile = util.promisify(fs.writeFile);
+
+const questionsStart=() => {
+    inquirer
+    .prompt([
+    {
+        type: "input",
+        message:"What is your name ? ",
+        name: 'employee',
+    },
+    {
+        type:'input',
+        message:"What is your ID number?",
+        name:'idnumber1',
+    },
+    {
+        type:'input',
+        message:"what is your email?",
+        name:'email1',
+    },
+])
+.then ((data)=>{
+    if (data.email1 = " ")
+    {
+        nextEmployee()
+    }
+})
+};
+
+const nextEmployee = () =>{
+     inquirer.prompt([
+     {
+        type: 'list',
+        message: 'Do you want to add another employee',
+        name: 'employee',
+        choices: ['Manager','Engineer','Intern','No more'],
+    }
+])
+.then ((data2)=>{
+    if (data2.employee ==='Manager'){
+        questionsManager()
+    }
+    else if (data2.employee === 'Engineer'){
+        questionsEngineer()
+    }
+    else if (data2.employee === 'Intern'){
+        questionsIntern()
+    }
+    else {
+        console.log ("Webpage being created")
+    }
+})
+};
+// manager questions
 const questionsManager = () =>{
-    return inquirer.prompt([
+     inquirer.prompt([
     {
         type: "input",
         message:"What is your name ? ",
@@ -16,12 +70,12 @@ const questionsManager = () =>{
     {
         type:'input',
         message:"What is your ID number?",
-        name:'idnumber',
+        name:'idnumber2',
     },
     {
         type:'input',
         message:"what is your email?",
-        name:'email',
+        name:'email2',
     },
     {
         type:'input',
@@ -29,26 +83,82 @@ const questionsManager = () =>{
         name:'office',
     },
    
-]);
-};
-
-const nextEmployee = () =>{
-    return inquirer.prompt([
-     {
-        type: 'list',
-        message: 'Do you want to add another employee',
-        name: 'employee',
-        choices: ['Manager','Engineer','Intern'],
+])
+.then ((data3)=>{
+    if (data3.office = " ")
+    {
+        nextEmployee()
     }
-]);
+})
 };
-
-
+// engineer questions
+const questionsEngineer = () =>{
+     inquirer.prompt([
+    {
+        type: "input",
+        message:"What is your name ? ",
+        name: 'engineer',
+    },
+    {
+        type:'input',
+        message:"What is your ID number?",
+        name:'idnumber3',
+    },
+    {
+        type:'input',
+        message:"what is your email?",
+        name:'email3',
+    },
+    {
+        type:'input',
+        message:'What is your Github username?',
+        name:'github',
+    },
+   
+])
+.then ((data4)=>{
+    if (data4.github = " ")
+    {
+        nextEmployee()
+    }
+})
+};
+// intern questions
+const questionsIntern = () =>{
+     inquirer.prompt([
+    {
+        type: "input",
+        message:"What is your name ? ",
+        name: 'intern',
+    },
+    {
+        type:'input',
+        message:"What is your ID number?",
+        name:'idnumber4',
+    },
+    {
+        type:'input',
+        message:"what is your email?",
+        name:'email4',
+    },
+    {
+        type:'input',
+        message:"what school did you attend?",
+        name:'school',
+    },
+   
+])
+.then ((data5)=>{
+    if (data5.school = " ")
+    {
+        nextEmployee()
+    }
+})
+};
 //  function to initialize app
 const init = () => {
-    questionsManager()
-    nextEmployee()
-      .then((data) => writeFileAsync('index.html', generateManager(data)))
+    questionsStart()
+      .then( ()=> {writeToFile('./team.html',)})
       .then(() => console.log('Successfully wrote to index.html'))
       .catch((err) => console.error(err));
   };
